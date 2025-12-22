55626bf78551412f39d1d64b6f1e9fa86efddebc
theedaud <theedaud@gmail.com>
2025-12-22 12:24:05 +0500

    fix: suppress hydration warning in RootLayout component
    - Updated the RootLayout component to include suppressHydrationWarning attribute in the html tag to prevent hydration issues during server-side rendering.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body 
         className={`${inter.variable} ${archivo.variable} font-sans antialiased`}
         suppressHydrationWarning
